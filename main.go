8ca8fa21683b4ef161cf0b972f65e1142bb1361b
tora0091 <tora0091@yahoo.co.jp>
2020-09-24 20:54:00 +0900

    product key to it is outside

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 
 	"./youtube"
 )
 
 func main() {
+	productKey, err := getProductKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	youtubeApi := youtube.NewYoutube()
+	youtubeApi.ProductKey = productKey
 	article, err := youtubeApi.Connect().GetArticle()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Print(article)
+}
+
+func getProductKey() (string, error) {
+	fp, err := os.Open("./product_key.conf")
+	if err != nil {
+		return "", err
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	scanner.Scan()
+	return scanner.Text(), nil
 }
